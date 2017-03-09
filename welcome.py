601e4a88a2553300016a7814c18fa12b6f8e18e8
thuykn <thuykn@us.ibm.com>
2017-03-09 16:04:57 -0500

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -29,24 +29,23 @@ import watson_developer_cloud
 import watson_developer_cloud.natural_language_understanding.features.v1 as \
 features
 
-app = Flask("NLU_Application")
+app = Flask("NLU Application")
 # In[3]:
 # new code from https://github.com/watson-developer-cloud/python-sdk/blob/master/examples/natural_language_understanding_v1.py	    
 nlu = watson_developer_cloud.NaturalLanguageUnderstandingV1(
-    version='2017-02-27',
-     username='YOUR SERVICE USERNAME',
-     password='YOUR SERVICE PASSWORD')
+       version='2017-02-27',
+     	username='YOUR SERVICE USERNAME',
+     	password='YOUR SERVICE PASSWORD')
 
 @app.route('/')
-	# new function	    
- 	def eval_default():
-       		 response = nlu.analyze(
-                	text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
-               		 'Superman fears not Banner, but Wayne.',
-                	features=[features.Entities(), features.Keywords()])
-       		return jsonify(response)
+# new function	    
+ def eval_default():
+       	response = nlu.analyze(
+ 		text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
+               	'Superman fears not Banner, but Wayne.',
+                features=[features.Entities(), features.Keywords()])
+       	return jsonify(response)
 
 #port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-	#app.run(host='0.0.0.0', port=int(port))
 app.run(host='0.0.0.0',debug=True,port=int(os.getenv('PORT',8080)))
