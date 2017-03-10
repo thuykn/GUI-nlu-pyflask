8374c7c88c9fdf9c89eac43c263995209eb1b303
thuykn <thuykn@us.ibm.com>
2017-03-10 10:38:57 -0500

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -37,12 +37,74 @@ nlu = watson_developer_cloud.NaturalLanguageUnderstandingV1(
        username=os.getenv('NATURAL_LANGUAGE_UNDERSTANDING_USERNAME'),
        password=os.getenv('NATURAL_LANGUAGE_UNDERSTANDING_PASSWORD'))
 
+# New code for testing NLU features:
 @app.route("/")
-def eval_default():
+def default_welcome():
+        return 'Welcome to the NLU Application!'
+
+@app.route("/entities")
+def eval_entities():
         response = nlu.analyze(
                 text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
                 'Superman fears not Banner, but Wayne.',
-                features=[features.Entities(), features.Keywords()])
+                features=[features.Entities()])
+        return jsonify(response)
+
+@app.route("/keywords")
+def eval_keywords():
+        response = nlu.analyze(
+                text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
+                'Superman fears not Banner, but Wayne.',
+                features=[features.Keywords()])
+        return jsonify(response)
+
+@app.route("/categories")
+def eval_categories():
+        response = nlu.analyze(
+                url='www.cnn.com',
+                features=[features.Categories()])
+        return jsonify(response) 
+
+@app.route("/concepts")
+def eval_concepts():
+        response = nlu.analyze(
+                text='Natural Language Understanding uses natural language processing to analyze text.',
+                features=[features.Concepts()])
+        return jsonify(response)
+
+@app.route("/emotion")
+def eval_emotion():
+        response = nlu.analyze(
+                text='I love apples, but I hate oranges.',
+                features=[features.Emotion(targets=['apples','oranges'])])
+        return jsonify(response) 
+
+@app.route("/metadata")
+def eval_metadata():
+        response = nlu.analyze(
+                url='https://www.ibm.com/blogs/think/2017/01/cognitive-grid/',
+                features=[features.MetaData()])
+        return jsonify(response)
+
+@app.route("/relations")
+def eval_relations():
+        response = nlu.analyze(
+                text='The Nobel Prize in Physics 1921 was awarded to Albert Einstein.',
+                features=[features.Relations()])
+        return jsonify(response)
+
+@app.route("/semantic_roles")
+def eval_semantic_roles():
+        response = nlu.analyze(
+                text='In 2011, Watson competed on Jeopardy!',
+                features=[features.SemanticRoles()])
+        return jsonify(response)
+
+@app.route("/sentiment")
+def eval_Sentiment():
+        response = nlu.analyze(
+                text='Thank you and have a nice day!',
+                features=[features.Sentiment()])
         return jsonify(response)
 
 #port = os.getenv('PORT', '5000')
